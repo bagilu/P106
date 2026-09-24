d84924c42484c7b1018dbcc185a5d5095351b1a4
C. M. Lin <bagilu@gmail.com>
2026-09-24 22:10:39 +0800

    P106 V2.2: add SDS v3.1 config template

---
+++ config-sample.js
@@ -0,0 +1,10 @@
+// P106 V2.2 SDS v3.1 deployment template.
+// Copy this file to config.js before deployment.
+// Do not commit service_role or any secret API key to GitHub.
+window.P106_CONFIG = {
+  SUPABASE_URL: 'https://YOUR_PROJECT_ID.supabase.co',
+  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY'
+};
+
+// Shared account lifecycle is handled by P130 Account Center:
+// https://bagilu.github.io/P130/
